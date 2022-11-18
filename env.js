9b3ee9e731f069e8f26fe75dad0b7ee9a8f85715
alexbvart <alexanderbvart@gmail.com>
2022-11-18 03:01:18 -0500

    declaration of environment variables

---
+++ env.js
@@ -0,0 +1,4 @@
+export const API_URL = import.meta.env.VITE_API_URL
+export const API_URL_CATEGORIES = `${import.meta.env.VITE_API_URL}/categories/`
+export const API_URL_PRODUCTS = `${import.meta.env.VITE_API_URL}/products`
+export const API_URL_USERS = `${import.meta.env.VITE_API_URL}/users`
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import {API_URL} from '../../../env'
 // import reactLogo from '../../assets/sayni.svg'
 
 export default function NavBar() {
   return (
     <h1>
-        NavBar
+        NavBar {API_URL}
     </h1>
   )
 }
